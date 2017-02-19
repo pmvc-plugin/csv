b395ca95fd586a03a5b80b962b08cd040b251922
Hill <hill@kimo.com>
2017-02-19 11:53:52 +0000

    Add callback

--- csv.php
+++ csv.php
@@ -9,34 +9,52 @@ class csv extends \PMVC\PlugIn
 {
     public function init()
     {
-       ini_set('auto_detect_line_endings',TRUE);
+        ini_set('auto_detect_line_endings', true);
     }
 
     public function get(
         $file,
         $charset=null,
         $ignore=null
-    )
-    {
-       $content = file_get_contents($file);
-       return $this->read($content, $charset, $ignore);
+    ) {
+    
+        $content = file_get_contents($file);
+        return $this->read($content, $charset, $ignore);
     }
 
     public function read(
         $content,
         $charset=null,
-        $ignore=null
-    )
-    {
-       $csv = new CsvReader(); 
-       if (isset($this['col'])) {
+        $ignore=null,
+        $callback=null
+    ) {
+    
+        $csv = new CsvReader(); 
+        if (isset($this['col'])) {
             $csv->setColumn($this['col']);
-       }
-       $csv->read($content, $charset, $ignore);
-       $data = [];
-       foreach ($csv as $v) {
-            $data[] = $v;
-       }
-       return $data;
+        }
+        $csv->read($content, $charset, $ignore);
+        $data = [];
+        foreach ($csv as $v) {
+            if (is_callable($callback)) {
+                $k = null;
+                $continue = call_user_func_array($callback,[&$v, &$k]);
+                if ($continue) {
+                    if (is_null($v)) {
+                        continue;
+                    }
+                    if ($k) {
+                        $data[$k] = $v;
+                    } else {
+                        $data[] = $v;
+                    }
+                } else {
+                    break;
+                }
+            } else {
+                $data[] = $v;
+            }
+        }
+        return $data;
     }
 }
